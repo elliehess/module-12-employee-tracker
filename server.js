fb3ffbbdf0bc0741d376903d4aea47f8d04a24cf
Ellie <ellie@Ellies-MBP.attlocal.net>
2023-02-27 13:29:12 -0800

    add switch statement

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
-const express = require('express');
+const inquirer = require('inquirer');
 require('dotenv').config();
-
-// Import and require mysql2
-const mysql = require('mysql2'); // create a connection
+const mysql = require('mysql2'); 
 
 const PORT = process.env.PORT || 3001;
-const app = express();
-
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 //Set up database connection 
 const db = mysql.createConnection(
@@ -21,3 +14,61 @@ const db = mysql.createConnection(
     },
     console.log(`Connected to the company_db database.`)
   );
+
+  const init = function () {
+    inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'options',
+            message: 'What would you like to do?',
+            choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add A Department', 'Add A Role', 'Add An Employee', 'Update An Employee Role', 'Quit']
+        },
+    ])
+    .then((answers) => {
+        handleOption(answers)
+    });
+  }
+
+  //use a switch statement to choose the correct function based on the users choice  
+function handleOption ({options}) {
+    switch (options) {
+        case 'View All Departments':
+            viewDepartments()               
+            break;
+        
+        case 'View All Roles':
+            viewRoles()        
+            break;
+        
+        case 'View All Employees':
+            viewEmployees()
+            break;
+        
+        case 'Add A Department':
+            addDepartment()
+            break;
+        
+        case 'Add A Role':
+            addRole()
+            break;
+            
+        case 'Add An Employee':
+            addEmployee()
+            break;
+            
+        case 'Update An Employee Role':
+            updateRole()
+            break;
+        
+        default:
+            console.log('Have a nice day! Goodbye.')
+            break;
+    }
+};
+
+
+
+
+
+
+  init();
